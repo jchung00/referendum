be9f1e30f75da3c23eefa96b0579786bf939fc4d
Jae Chung <jae.chung99@gmail.com>
2019-03-19 19:30:57 -0400

    more code

--- src/referendum.cpp
+++ src/referendum.cpp
@@ -1,2 +1,85 @@
 #include "referendum.hpp"
 
+namespace referendum{
+
+    void referendum_contract::makeproposal(name creator, const string& proposal_name, const string& content){
+        require_auth(creator);
+        eosio_assert(proposal_name.size() < 200, "proposal name is too long");
+        eosio_assert(content.size() < 16384, "proposal content is too long");
+
+        proposals _proposals(_self, _self.value);
+
+        auto proposal_counter = _counter.get();
+
+        _proposals.emplace(creator, [&](auto& proposal){
+            proposal.id = proposal_counter.global_id;
+            proposal.creator = creator;
+            proposal.proposal_name = proposal_name;
+            proposal.content = content;
+            proposal.yes = 0;
+            proposal.no = 0;
+        });
+
+        proposal_counter.global_id++;
+        _counter.set(proposal_counter, _self);
+    }
+
+    void referendum_contract::transfer(uint64_t sender, uint64_t receiver){
+        auto transfer_data = eosio::unpack_action_data<st_transfer>();
+
+        if(transfer_data.from == get_self()){
+            return;
+        }
+
+        eosio_assert(transfer_data.from == get_self() || transfer_data.to == get_self(), "Invalid transfer");
+
+        eosio_assert(transfer_data.quantity.is_valid(), "Invalid asset");
+
+        voters _voters(_self, _self.value);
+
+        auto itr = _voters.find(voter.value);
+
+        if(itr == _voters.end()){
+            _voters.emplace(_self, [&](auto& voter){
+                voter.name = sender;
+                voter.amount = transfer_data.quantity.amount;
+            });
+        }
+        else{
+            _voters.modify(itr, 0, [&](auto& voter){
+                voter.amount += transfer_data.quantity.amount;
+            });
+        }
+    }
+
+    void referendum_contract::vote(name voter, uint64_t id, bool is_agree){
+        require_auth(voter);
+        voters _voters(_self, _self.value);
+
+        auto itr = _voters.find(voter.value);
+        eosio_assert(itr != _voters.end(), "must transfer tokens first");
+
+        auto idx_index = _proposals.get_index<name("idx")>();
+        auto itr_proposal = idx_index.find(id);
+        eosio_assert(itr_proposal != idx_index.end(), "Proposal not found");
+
+        //auto itr_voted_proposals = std::find((*itr).proposal_ids.begin(), (*itr).proposal_ids.end(), id);
+
+        proposals _proposals(_self, _self.value);
+
+        _voters.modify(itr, 0, [&](auto& voter){
+
+        });
+
+        /*_proposals.modify(itr_proposal, 0, [&](auto& proposal){
+            if(is_agree){
+                proposal.yes += (*itr).amount;
+            }
+            else{
+                proposal.no += (*itr).amount;
+            }
+        })*/
+    }
+} //referendum
+
+EOSIO_DISPATCH(referendum::referendum_contract, (makeproposal)(transfer))
--- src/referendum.hpp
+++ src/referendum.hpp
@@ -1,2 +1,99 @@
 #pragma once
 
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/time.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/singleton.hpp>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+namespace referendum{
+    using eosio::const_mem_fun;
+    using eosio::datastream;
+    using eosio::indexed_by;
+    using eosio::name;
+    using eosio::asset;
+    using eosio::time_point_sec;
+    using std::function;
+    using std::string;
+    using std::vector;
+
+    class [[eosio::contract("referendum_contract")]] referendum_contract : public eosio::contract{
+
+        public:
+            using contract::contract;
+
+            referendum_contract( name s, name code, datastream<const char*> ds )
+                    :contract(s,code,ds),
+                     _counter(_self, _self.value){}
+            //explicit referendum_contract(uint64_t get_self());
+
+            [[eosio::action]]
+        void makeproposal(name creator, const string& proposal_name, const string& content);
+
+        [[eosio::action]]
+        void transfer(uint64_t sender, uint64_t receiver);
+
+        [[eosio::action]]
+        void vote(name voter, uint64_t id, bool is_agree);
+
+        private:
+            struct account {
+                asset    balance;
+
+                uint64_t primary_key() const { return balance.symbol.code().raw(); }
+            };
+
+            typedef eosio::multi_index<name("accounts"), account> accounts;
+
+            // taken from eosio.token.hpp
+            struct st_transfer {
+                name  from;
+                name  to;
+                asset         quantity;
+                std::string   memo;
+            };
+
+            struct [[eosio::table]] proposal{
+                uint64_t id;
+                name creator;
+                string proposal_name;
+                string content;
+                int64_t yes;
+                int64_t no;
+
+                uint64_t primary_key() const { return creator.value; }
+                uint64_t by_id() const { return static_cast<uint64_t>(id); }
+            };
+
+            typedef eosio::multi_index< name("proposals"), proposal,
+                    indexed_by< name("idx"), const_mem_fun<proposal, uint64_t, &proposal::by_id>  >
+            > proposals;
+
+            //typedef eosio::multi_index<name("proposals"), proposal> proposals;
+
+            struct [[eosio::table]] voter{
+                name voter;
+                int64_t amount;
+                vector<int64_t> agreed_proposal_ids;
+                vector<int64_t> disagreed_proposal_ids;
+
+                uint64_t primary_key() const { return voter.value; }
+            };
+
+            typedef eosio::multi_index<name("voters"), voter> voters;
+
+            struct counter{
+                uint64_t global_id = 0;
+
+                EOSLIB_SERIALIZE(counter, (global_id));
+            };
+
+            typedef eosio::singleton< name("counter"), counter > counter_singleton;
+
+            counter_singleton _counter;
+
+    };
+} //referendum
